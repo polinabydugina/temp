e6a5334fdd6cd64903773cbf8353b40681b0de15
polinabydugina <xch.victor@mail.ru>
2023-11-22 20:24:51 +0300

    Добавьте файлы проекта.

---
+++ temp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+        while (true)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n > 3)
+            {
+                int[] temps = new int[n];
+                for (int i = 0; i < n; i++)
+                {
+                temps[i] = Convert.ToInt32(Console.ReadLine());
+                }
+
+                for (int i = 1; i < n - 1; i++)
+                {
+                    if (temps[i] > temps[i - 1] && temps[i] > temps[i + 1])
+                    {
+                    Console.WriteLine("day {0} : {1} (max)", i + 1, temps[i]);
+                    }
+                    if (temps[i] < temps[i - 1] && temps[i] < temps[i + 1])
+                    {
+                        Console.WriteLine("day {0} : {1} (min)", i + 1, temps[i]);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Число дней не должно быть меньше 3");
+            }
+        }
